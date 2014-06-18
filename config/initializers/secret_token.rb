e3f80385725a3f26c73d636a2ab9de5bff5d61d4
isheeta.dabhi-BTC <isheeta.dabhi@beaverlogic.com>
2014-06-18 10:02:22 +0530

    abc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Abc::Application.config.secret_key_base = 'ae12d90824e9d135a4df5d21f3db444fd7e6aee46ad61b3b3b9e3333ad39b7f7d9703bf90f1a044cfc6502a8e36c3c87d4686f4a0c04aae87dba7302c7a4d228'
